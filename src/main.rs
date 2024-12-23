f62d462d482a020049533f7421d7cf3396083e5e
chris-dem <christosdeme@gmail.com>
2024-12-23 20:52:50 +0200

    Add files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate blas_src;
+extern crate ndarray;
+extern crate ndarray_linalg;
+
+use ndarray::prelude::*;
+use ndarray_linalg::generate::random;
+use ndarray_linalg::solve::Determinant;
+
 fn main() {
-    println!("Hello, world!");
+    let arr: Array2<f64> = random((3, 3));
+    let arr2: Array2<f64> = random((3, 3));
+    let dd = arr.dot(&arr2);
+    println!("{:?}", dd.det());
 }
